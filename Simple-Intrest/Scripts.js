10cd96b6751db4ed5abec06d6911d8044ae42526
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2022-11-29 21:07:47 +0530

    Update Scripts.js

--- Simple-Intrest/Scripts.js
+++ Simple-Intrest/Scripts.js
@@ -1,20 +1,28 @@
-let calBtn = document.getElementById("calculate");
-calBtn.addEventListener("click",Calculate());
-  function Calculate(){
-    let p = parseInt(document.getElementById("Principal").value);
-    let r = parseInt(document.getElementById("Rate").value);
-    let t = parseInt(document.getElementById("Time").value);
+  let calBtn = document.getElementById("Calculate");
+  calBtn.addEventListener("click",Calculate);
+  window.addEventListener("load",Calculate);
+    function Calculate(){
+    let P = parseInt(document.getElementById("Principal").value);
+    let R = parseInt(document.getElementById("Rate").value);
+    let T = parseInt(document.getElementById("Time").value);
     let duration = (document.getElementById("Duration").value);
 
-    let simpleInt = duration =="Year" ? (p*r*t) / 100 : (p*r*t) / 1200;
+    let simpleInt = 0;
+    let Amt = 0;
 
-    console.log(simpleInt);
+    ///// Calculation of Intrest According to The Duration ///
+    if(duration == "Year"){
+      simpleInt = (P*R*T)/100;
+    }
+    else{
+      simpleInt = (P*R*T)/1200;
+    }
 
-    let amt = p + simpleInt;
-    console.log(amt);
+    Amt = P + simpleInt;
 
-    document.getElementById("Result").innerHTML = `<div>Principal Amount: <span> ${p.toFixed(2)} </span></div>
-  <div>Total Interest: <span>${simpleInt.toFixed(2)}</span></div>
-  <div>Total Amount: <span>${amt.toFixed(2)}</span></div>`;
+    let Result = document.getElementById("Result");
+    Result.innerHTML =  `<div>Principal Amount: <span>${P.toFixed(2)}</span></div>
+    <div>Total Interest: <span>${simpleInt.toFixed(2)}</span></div>
+    <div>Total Amount: <span>${Amt.toFixed(2)}</span></div>`;
   };
-
+  

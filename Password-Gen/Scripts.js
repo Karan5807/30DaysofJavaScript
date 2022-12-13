40aabacf0c4f0f73b924f7ce884e27693704350e
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2022-12-13 23:38:09 +0530

    Create Scripts.js

---
+++ Password-Gen/Scripts.js
@@ -0,0 +1,7 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+={]}]"?/|\\.,123456789';
+let Output = document.getElementById("Output");
+
+function gen(){
+    let length = document.getElementById("length").value;
+    document.getElementById("length-val").textContent = length;
+}

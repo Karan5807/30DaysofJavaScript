972288cc944f37648741a343c8112e303d7d27a8
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2022-12-15 03:08:02 +0530

    Create Scripts.js

---
+++ FAQ/Scripts.js
@@ -0,0 +1,16 @@
+let Toggle = document.getElementsByClassName("toggle");
+console.log(Toggle);
+
+let Content = document.getElementsByClassName("content");
+console.log(Content);
+
+let Icon = document.getElementsByClassName("icon");
+console.log(Icon);
+
+for(let i=0; i<Toggle.length; i++){
+    Toggle[i].addEventListener('click', ()=>{
+        if(parseInt(Content[i].style.height) != Content[i].scrollHeight){
+            Content.style.height = Content[i].scrollHeight + "px";
+        }
+    });
+}

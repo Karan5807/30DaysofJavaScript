15923c3bc492122a1eaeee2a1b58d6b8caf55a83
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2022-12-15 03:13:37 +0530

    Update Scripts.js

--- FAQ/Scripts.js
+++ FAQ/Scripts.js
@@ -10,7 +10,7 @@ console.log(Icon);
 for(let i=0; i<Toggle.length; i++){
     Toggle[i].addEventListener('click', ()=>{
         if(parseInt(Content[i].style.height) != Content[i].scrollHeight){
-            Content.style.height = Content[i].scrollHeight + "px";
+            Content[i].style.height = Content[i].scrollHeight + "px";
         }
     });
 }

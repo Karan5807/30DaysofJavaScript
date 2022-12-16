4dbada954b48e4dd86015730549ef90b9bba23e5
Karan Singh <90771675+Karan5807@users.noreply.github.com>
2022-12-17 03:35:17 +0530

    Update Scripts.js

--- Love-Calculator/Scripts.js
+++ Love-Calculator/Scripts.js
@@ -13,9 +13,21 @@ function calLove(){
     else{
         var random = Math.floor(Math.random()*100);
         Result.textContent = random;
-
-        if(random > 0 || random < 10){
-            Result.textContent=random+" Close the Browser! Right Now";
+        
+        if(random > 0 && random < 10){
+            Result.textContent= "Love Percentage is "+random+" Close the Browser! Right Now";
+        }
+        else if(random>10 && random<30){
+            Result.textContent = "Love Percentage is "+random;
+        }
+        else if(random>30 && random<60){
+            Result.textContent = "Love Percentage is "+random;
+        }
+        else if(random>60 && random<80){
+            Result.textContent = "Love Percentage is "+random+ " Lovely RelationShip";
+        }
+        else if(random>80 && random<100){
+            Result.textContent = "Love Percentage is "+random+ " Marry as soon as Possible";
         }
     }
 }
